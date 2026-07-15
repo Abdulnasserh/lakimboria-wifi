265d8e41df9f23af0f9d2319c48abab6d9565290
Abdulnasserh <abdulbnasserh06@gmail.com>
2026-07-15 19:57:31 +0300

    Swahili captive portal with Lakimboria WiFi branding

--- hotspot/conf.js
+++ hotspot/conf.js
@@ -1,15 +1,10 @@
 var config = {
-
-loginvc : "Enter Voucher Code then click Connect.",
-loginup : "Enter Username and Password <br> then click Connect.",
-voucherCode : "Voucher Code",
-setCase : "none", // lowercase, uppercase or none
-defaultMode : "voucher", // voucher or member
-theme : "default", // default, dark, lite
-
-// status expire
-
-url : "http://localhost:8080", // url server Mikhmon
-SessionName : "hotspot", 
-
+  loginvc : "Weka Kodi ya Vocha kisha bonyeza Unganisha.",
+  loginup : "Weka Jina la Mtumiaji na Nywila kisha bonyeza Unganisha.",
+  voucherCode : "Kodi ya Vocha",
+  setCase : "none",
+  defaultMode : "voucher",
+  theme : "default",
+  url : "http://localhost:8080",
+  SessionName : "hotspot",
 }
